c25f836413192c6ce18c24ed574d4adc9efd70eb
Majid Sepandarand <majid_sp2007@yahoo.com>
2024-07-09 15:37:24 +0200

    completed chapter 16

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -31,8 +31,7 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
 
-        console.log('Fetching latest invoices data...');
-    await new Promise((resolve) => setTimeout(resolve, 10000));
+    console.log('Fetching latest invoices data...');
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
